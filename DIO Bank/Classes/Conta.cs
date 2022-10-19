44397da2ad63d824525b6e7e5878ed32f53a846c
Lucas Patricio <lucas_patricio@outlook.com.br>
2022-10-19 03:21:24 -0300

    Montado a base do projeto utilizando modelo DIO como inspiração

---
+++ DIO Bank/Classes/Conta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO_Bank.Classes
+{
+    public class Conta
+    {
+        public string NumeroConta { get; private set; }
+        private string NomeCliente { get; set; }
+        private decimal Saldo { get; set; }
+        private decimal ChequeEspecial { get; set; }
+        private TipoConta Tipo { get; set; }
+
+        public Conta(string numeroConta, string nomeCliente, decimal saldoInicial, decimal chequeEspecial, TipoConta tipoConta)
+        {
+            this.NumeroConta = numeroConta;
+            this.NomeCliente = nomeCliente;
+            this.Saldo = saldoInicial;
+            this.ChequeEspecial = chequeEspecial;
+            this.Tipo = tipoConta;
+        }
+
+
+        public bool Sacar(decimal valorSaque)
+        {
+            if (valorSaque > (this.Saldo + this.ChequeEspecial))
+            {
+                Console.WriteLine("Saldo Insuficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+            Console.WriteLine($"O saldo atual após o saque na conta N° {this.NumeroConta} é de R${this.Saldo}");
+            return true;
+        }
+
+        public void Depositar(decimal valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine($"O saldo atual após o deposito na conta N° {this.NumeroConta} é de R${this.Saldo}");
+        }
+
+        public bool Transferir(Conta contaDestino, decimal valorTransferir)
+        {
+            if (this.Sacar(valorTransferir))
+            {
+                contaDestino.Depositar(valorTransferir);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"N° Conta: {this.NumeroConta}");
+            sb.AppendLine($"Nome: {this.NomeCliente}");
+            sb.AppendLine($"Saldo: R$ {this.Saldo}");
+            sb.AppendLine($"Cheque Especial: R$ {this.ChequeEspecial}");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ DIO Bank/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using DIO_Bank.Classes;
+
+
+namespace DIO_Bank
+{
+    internal class Program
+    {
+        private static List<Conta> listaContas = new List<Conta>();
+        private static List<string> listaLog = new List<string>();
+        static void Main(string[] args)
+        {
+            //Conta contaCorrente = new Conta("203040", "Lucas Patricio", 200, 100, TipoConta.PessoaFisica);
+
+            MenuPrincipal();
+
+        }
+
+        static void MenuPrincipal()
+        {
+            string opcao = "";
+            while (opcao != "6")
+            {
+                Console.WriteLine("Bem vindo ao Banco DIO!");
+                Console.WriteLine("| 1 | Nova Conta");
+                Console.WriteLine("| 2 | Sacar");
+                Console.WriteLine("| 3 | Depositar");
+                Console.WriteLine("| 4 | Transferir");
+                Console.WriteLine("| 5 | Extrato Geral");
+                Console.WriteLine("| 6 | Sair");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        NovaConta();
+                        break;
+                    case "2":
+                        SacarConta();
+                        break;
+                    case "3":
+                        DepositarConta();
+                        break;
+                    case "4":
+                        Transferir();
+                        break;
+                    case "5":
+                        ExtratoTransacoes();
+                        break;
+                }
+            }
+        }
+
+        private static void NovaConta()
+        {
+            Console.Clear();
+            Console.WriteLine("| NOVA CONTA |");
+
+            Console.Write("N° Conta: ");
+            string numeroConta = Console.ReadLine();
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Saldo Inicial: ");
+            decimal saldoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Limite Cheque Especial: ");
+            decimal chequeEspecial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Tipo Conta ( 1 - PF | 2 - PJ ): ");
+            int tipoConta = Convert.ToInt32(Console.ReadLine());
+
+            Conta conta = new Conta(numeroConta: numeroConta,
+                nomeCliente: nome,
+                saldoInicial: saldoInicial,
+                chequeEspecial: chequeEspecial,
+                tipoConta: (TipoConta)tipoConta);
+
+            listaContas.Add(conta);
+            listaLog.Add(conta.ToString());
+        }
+
+        private static void SacarConta()
+        {
+            Console.Clear();
+            Console.WriteLine("| SAQUE |");
+
+            Console.Write("N° Conta: ");
+            string numeroConta = Console.ReadLine();
+            Console.Write("Valor Saque: ");
+
+            decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+            Conta contaRecebida = listaContas.FirstOrDefault(x => x.NumeroConta == numeroConta);
+            if (contaRecebida.Sacar(valorSaque: valorSaque))
+            {
+                listaLog.Add($"Saque: R$ {valorSaque}, Conta N° {numeroConta}");
+            }
+        }
+
+        private static void DepositarConta()
+        {
+            Console.Clear();
+            Console.WriteLine("| DEPOSITO |");
+
+            Console.Write("N° Conta: ");
+            string numeroConta = Console.ReadLine();
+
+            Console.Write("Valor Deposito: ");
+            decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+
+            Conta contaRecebida = listaContas.FirstOrDefault(x => x.NumeroConta == numeroConta);
+            contaRecebida.Depositar(valorDeposito: valorDeposito);
+            listaLog.Add($"Deposito: R$ {valorDeposito}, Conta N° {numeroConta}");
+        }
+
+        private static void Transferir()
+        {
+            Console.Clear();
+            Console.WriteLine("| TRANSFERIR |");
+
+            Console.Write("N° Conta Origem: ");
+            string numeroContaOrigem = Console.ReadLine();
+
+            Console.Write("N° Conta Destino: ");
+            string numeroContaDestino = Console.ReadLine();
+
+            Console.Write("Valor Transferencia: ");
+            decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
+
+            Conta contaOrigem = listaContas.FirstOrDefault(x => x.NumeroConta == numeroContaOrigem);
+            Conta contaDestino = listaContas.FirstOrDefault(x => x.NumeroConta == numeroContaDestino);
+
+            if (contaOrigem.Transferir(contaDestino: contaDestino,
+                valorTransferir: valorTransferencia))
+            {
+                listaLog.Add($"Transferido: R$ {valorTransferencia}, Conta Origem N° {numeroContaOrigem}, Conta Destino N° {numeroContaDestino}");
+            }
+        }
+
+        private static void ExtratoTransacoes()
+        {
+            Console.WriteLine("| EXTRATO |");
+            foreach(string registro in listaLog)
+            {
+                Console.WriteLine(registro);
+            }
+            Console.ReadKey();
+        }
+    }
+}
